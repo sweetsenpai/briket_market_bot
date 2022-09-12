6ed3578cb172ebefd908948627a02a2efed974b4
Sweet_Sempai <chcolatemilk00@gmail.com>
2022-09-12 20:24:58 +0300

    order to residents

--- briket_DB/order_db.py
+++ briket_DB/order_db.py
@@ -8,6 +8,17 @@ def push_order(user_id: int):
     cart = sh_cart.find_one({"user_id": user_id})
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     cart['time'] = time
+    cart['status'] = 'new'
+    cart['order_num'] = datetime.now().microsecond
     orders_db.insert_one(cart)
+    sh_cart.delete_one({"user_id": user_id})
     return
+
+
+def send_order_residents(order_num: int):
+    orders_db.find_one({"order_num": order_num})
+    return
+
+
+push_order(352354383)
 
