64449bdb858e7e846bc3ee980340ee154ab5040c
Sweet_Sempai <chcolatemilk00@gmail.com>
2022-09-20 23:28:59 +0300

    последние пракви перед beta

--- bot_body/main.py
+++ bot_body/main.py
@@ -98,7 +98,6 @@ def main() -> None:
     application.add_handler(InlineQueryHandler(menu.inline_query))
     application.add_handler(reg_user)
     application.add_handler(CallbackQueryHandler(call_back_handler))
-    application.setWebhook(url=f'https://brikettestbot.herokuapp.com/{bot_key}', )
     application.run_webhook(port=PORT, url_path=bot_key, webhook_url=f'https://brikettestbot.herokuapp.com/')
 
 
