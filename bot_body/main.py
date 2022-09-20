4aa7817d18862c225038284e1a586e7d37f233d8
Sweet_Sempai <chcolatemilk00@gmail.com>
2022-09-20 23:01:49 +0300

    последние пракви перед beta

--- bot_body/main.py
+++ bot_body/main.py
@@ -20,7 +20,7 @@ PORT = int(os.environ.get('PORT', '8443'))
 
 
 async def main() -> None:
-    application = Application.builder().token(bot_key).build()
+    application = Application.builder().token(bot_key).build().
 
     reg_user = ConversationHandler(
         entry_points=[CommandHandler("start", rg.start)],
@@ -98,7 +98,7 @@ async def main() -> None:
     application.add_handler(InlineQueryHandler(menu.inline_query))
     application.add_handler(reg_user)
     application.add_handler(CallbackQueryHandler(call_back_handler))
-    await application.bot.setWebhook(url=f'https://brikettestbot.herokuapp.com/{bot_key}', )
+    application.bot.setWebhook(url=f'https://brikettestbot.herokuapp.com/{bot_key}', )
     application.run_webhook(port=PORT, url_path=bot_key, webhook_url=f'https://brikettestbot.herokuapp.com/')
 
 
