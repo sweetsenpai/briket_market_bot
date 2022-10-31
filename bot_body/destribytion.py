4230622bbc2cbf9a12aeb5a4e8ef10647945e557
Sweet_Sempai <chcolatemilk00@gmail.com>
2022-10-31 20:04:28 +0300

    host

--- bot_body/destribytion.py
+++ bot_body/destribytion.py
@@ -8,6 +8,7 @@ from telegram import (Update)
 from briket_DB.customers import read_all
 from text_integration.pastebin_integration import get_text_api
 from time import sleep
+import asyncio
 TEXT_DIST = range(1)
 
 
@@ -20,11 +21,16 @@ async def start_distribution(update: Update, context: ContextTypes.DEFAULT_TYPE)
 
     text_distribution = update.message.text
     await update.message.reply_text(text=get_text_api('n3M4zWjt'))
-    for customer in read_all():
-        try:
-            await context.bot.sendMessage(chat_id=customer['chat_id'], text=text_distribution)
-        except telegram.error.BadRequest:
-            pass
+    context.application.create_task(
+        asyncio.gather(
+            *(
+                context.bot.send_message(
+                    chat_id=customer['chat_id'], text=text_distribution, rate_limit_args={'priority': -1}
+                )
+                for customer in read_all()
+            )
+        )
+    )
     return ConversationHandler.END
 
 
