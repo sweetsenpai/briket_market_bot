fe629cef63af81aa39554187339d3d8fc4dac9e8
sweetsenpai <32019970+sweetsenpai@users.noreply.github.com>
2023-02-02 20:58:59 +0300

    mac_chees img parcer

--- parcer/mac_cheese.py
+++ parcer/mac_cheese.py
@@ -1,18 +1,29 @@
 import requests
 from selenium import webdriver
+from selenium.webdriver.support.select import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup as bs
 import codecs
 import re
+from time import sleep
 
-# url = 'https://disk.yandex.ru/d/VjfZRBgaGJvp5Q/Горячие%20блюда/Запеченный%20Mac%26Cheese'
-# options = Options()
+url = 'https://disk.yandex.ru/d/VjfZRBgaGJvp5Q/Горячие%20блюда/Запеченный%20Mac%26Cheese'
+options = Options()
 # options.headless = True
 # options.add_experimental_option("detach", True)
-# options.add_argument("--window-size=1920,1200")
-# DRIVER_PATH = '/chromedriver_win32/chromedriver.exe'
-# driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-# driver.get(url)
+options.add_argument("--window-size=1920,1200")
+DRIVER_PATH = '/chromedriver_win32/chromedriver.exe'
+driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+driver.get(url)
+card_list = driver.find_elements(By.CLASS_NAME, 'public-grid-item-preview')
+for card in card_list:
+    card.click()
+    print(driver.current_url)
+    sleep(1)
+    card.send_keys(Keys.ESCAPE)
+
+
 
 # html = driver.page_source
 # soup = bs(html, 'html.parser')
@@ -34,10 +45,6 @@ import re
 #     titel_clean = titel_raw.replace(price[0], '')
 #     dish_data['titel'].append(titel_clean)
 #     dish_data['price'].append(price)
-key = requests.get('https://oauth.yandex.ru/authorize?response_type=code&client_id=b2e5f7ebd9d14e0bac47853c3fa30a55')
-
-print(key.text)
-
 
 
 
